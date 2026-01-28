dab87fa5bcfc71b19a3ea4461ffa350a58ee29ec
VIKAS S <155833208+Vikas1923@users.noreply.github.com>
2026-01-28 16:15:00 +0530

    Added CORSMiddleware

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,18 @@
 
 from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Secure Chat",
     version="0.1"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
 )
 
 @app.get("/health", status_code=status.HTTP_200_OK)
